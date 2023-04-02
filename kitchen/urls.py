73c1b621063f10581a4a5565e5581df422d9f550
Andrii Skryl <a.skryl92@gmail.com>
2023-04-02 15:30:40 +0300

    Created urls for DishType views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import index, DishTypeListView, DishTypeCreateView, DishTypeUpdateView, DishTypeDeleteView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list",
+    ),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create",
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update",
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete",
+    ),
 ]
 
 app_name = "kitchen"
