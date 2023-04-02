c8d3ed23200962bc4c8588b168115ebf8ecc6b89
Andrii Skryl <a.skryl92@gmail.com>
2023-04-02 14:19:45 +0300

    Fixed namespace bug in global urls, created url for home page and edited styles.css

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from kitchen.views import index
+
+urlpatterns = [
+    path("", index, name="index")
+]
 
 app_name = "kitchen"
--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("kitchen.urls"), namespace="kitchen")
+    path("", include("kitchen.urls", namespace="kitchen"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
