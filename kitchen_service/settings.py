1c55574a61d8e47183a827d51f19a118669bdec7
Andrii Skryl <a.skryl92@gmail.com>
2023-04-08 11:48:20 +0300

    Implemented stylization of the project

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "kitchen.context_processors.cfg_assets_root"
             ],
         },
     },
@@ -136,3 +137,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ASSETS_ROOT = "/static/assets/"
